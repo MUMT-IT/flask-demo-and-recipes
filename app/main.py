84ea32f58ea9132707b88448f666f56bcd4fd5b8
Likit Preeyanon <likit.pre@mahidol.edu>
2022-06-12 20:28:40 +0700

    added flask migrate tool
    initial db migration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 from models import *
 
@@ -9,8 +10,8 @@ load_dotenv()
 app = Flask(__name__)  # file name
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
 db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/', methods=['GET'])
